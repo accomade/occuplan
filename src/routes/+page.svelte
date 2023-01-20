1d5efed3e145fb2ab3e9418c3663f9ae96e8578e
Dirk Porsche <dirkporsche78@googlemail.com>
2023-01-20 22:39:08 +0100

    fixed index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <h1>Occuplan - the easy calendar component</h1>
 
-<div>
-<a href="/separated">Direct usage of component</a>
-<a href="/integraded">Dynamic/responsive usage of components</a>
-</div>
+<ul>
+  <li>
+    <a href="/separated">Direct usage of component</a>
+  </li>
+  <li>
+    <a href="/integrated">Dynamic/responsive usage of components</a>
+  </li>
+</ul>
+
