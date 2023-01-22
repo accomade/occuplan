80e2d3321aaccf516f51e52502923b85b644ca19
Dirk Porsche <dirkporsche78@googlemail.com>
2023-01-22 21:35:22 +0100

    exporting types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,8 +3,15 @@ import OccuPlanGrid from "./components/OccuPlanGrid.svelte"
 import OccuPlanRows from "./components/OccuPlanRows.svelte"
 import OccuPlanWrapper from "./components/OccuPlanWrapper.svelte"
 
+
 export { 
   OccuPlanGrid,
   OccuPlanRows,
   OccuPlanWrapper,
 }
+
+export type { 
+  WeekdayLabels, 
+  MonthLabels, 
+  I18n
+} from './types/i18n'
