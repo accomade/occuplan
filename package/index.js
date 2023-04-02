74fa9df37f8ff232d22c5a93eb6dfd42809a7146
Dirk Porsche <dirkporsche78@googlemail.com>
2023-04-02 10:55:17 +0200

    cleanup

--- package/index.js
+++
@@ -1,6 +0,0 @@
-// Reexport your entry components here
-import OccuPlanGrid from "./components/OccuPlanGrid.svelte";
-import OccuPlanRows from "./components/OccuPlanRows.svelte";
-import OccuPlanWrapper from "./components/OccuPlanWrapper.svelte";
-import OccuPlanAvailableInfo from "./components/OccuPlanAvailableInfo.svelte";
-export { OccuPlanGrid, OccuPlanRows, OccuPlanWrapper, OccuPlanAvailableInfo, };
