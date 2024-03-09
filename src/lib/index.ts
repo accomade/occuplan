49945e934fee836329fed056e0b4652c59f449a1
Dirk Porsche <dirkporsche78@googlemail.com>
2024-03-09 22:03:19 +0100

    expose getEvents

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,12 +4,14 @@ import OccuPlanRows from "./components/OccuPlanRows.svelte"
 import OccuPlanWrapper from "./components/OccuPlanWrapper.svelte"
 import OccuPlanAvailableInfo from "./components/OccuPlanAvailableInfo.svelte"
 
+import { getEvents } from './helpers/readICS'
 
 export { 
   OccuPlanGrid,
   OccuPlanRows,
   OccuPlanWrapper,
   OccuPlanAvailableInfo,
+  getEvents as getEventsFromICal,
 }
 
 export type { 
